b37b24f5f38487ed97285c4106c34eb5c95d9368
hyukkyu <kms9404111@naver.com>
2024-06-21 14:03:17 +0900

    백준 3273번 최초 커밋

---
+++ src/hyukkyu/boj/array/_3273/Main.java
@@ -0,0 +1,45 @@
+package hyukkyu.boj.array._3273;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String n = br.readLine();
+		String[] array = new String[Integer.parseInt(n)];
+		array = br.readLine().split(" ");
+
+		int x = Integer.parseInt(br.readLine());
+		int[] array2 = Arrays.stream(array).mapToInt(Integer::parseInt).toArray();
+		
+		Arrays.sort(array2);
+		
+		int count = 0;
+		int sum = 0;
+		int start = 0;
+		int end = array2.length - 1;
+		
+		while(start < end) {
+			sum = array2[start] + array2[end];
+			
+			if (sum == x) {
+				count++;
+				start++;
+				end--;
+			} else if (sum < x) {
+				start++;
+			} else {
+				end--;
+			}
+		}
+		
+		System.out.println(count);
+
+	}
+
+}
